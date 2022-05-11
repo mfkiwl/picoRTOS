bc7687cd5034350800fae79c43a47656d34ba83c
Joachim Naulet <joachim.naulet@koenigsegg.com>
2022-05-11 17:46:39 +0200

    ipc: fixed splint complain about trylock(s)

--- ipc/picoRTOS_futex.h
+++ ipc/picoRTOS_futex.h
@@ -8,10 +8,10 @@ typedef picoRTOS_atomic_t picoRTOS_futex_t;
 /* macro for static init */
 #define PICORTOS_FUTEX_INITIALIZER (picoRTOS_futex_t)0
 
-void picoRTOS_futex_init(/*@notnull@*/ /*@out@*/ picoRTOS_futex_t *futex);  /* init futex */
+void picoRTOS_futex_init(/*@notnull@*/ /*@out@*/ picoRTOS_futex_t *futex);      /* init futex */
 
-int picoRTOS_futex_trylock(/*@notnull@*/ picoRTOS_futex_t *futex);          /* try ot lock futex */
-void picoRTOS_futex_lock(/*@notnull@*/ picoRTOS_futex_t *futex);            /* lock futex */
-void picoRTOS_futex_unlock(/*@notnull@*/ picoRTOS_futex_t *futex);          /* unlock futex */
+/*@unused@*/ int picoRTOS_futex_trylock(/*@notnull@*/ picoRTOS_futex_t *futex); /* try ot lock futex */
+void picoRTOS_futex_lock(/*@notnull@*/ picoRTOS_futex_t *futex);                /* lock futex */
+void picoRTOS_futex_unlock(/*@notnull@*/ picoRTOS_futex_t *futex);              /* unlock futex */
 
 #endif
