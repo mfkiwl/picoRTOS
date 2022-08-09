1fc747ff2c9395dd1d0fff5c5ed856ad612b3542
Joachim Naulet <joachim.naulet@koenigsegg.com>
2022-08-09 13:32:39 +0200

    c28x: compatibility with COFF & EABI formats

--- arch/c2000/c28x/picoRTOS_portasm.asm
+++ arch/c2000/c28x/picoRTOS_portasm.asm
@@ -1,10 +1,22 @@
-	.ref picoRTOS_tick
-	.ref picoRTOS_switch_context
-	
-	.global arch_INT14
-	.global arch_RTOSINT
-	.global arch_start_first_task
-	.global arch_test_and_set
+	.if __TI_EABI__
+	.asg picoRTOS_tick, _picoRTOS_tick
+	.asg picoRTOS_switch_context, _picoRTOS_switch_context
+	.endif
+
+	.ref _picoRTOS_tick
+	.ref _picoRTOS_switch_context
+
+	.if __TI_EABI__
+	.asg arch_INT14, _arch_INT14
+	.asg arch_RTOSINT, _arch_RTOSINT
+	.asg arch_start_first_task, _arch_start_first_task
+	.asg arch_test_and_set, _arch_test_and_set
+	.endif
+
+	.global _arch_INT14
+	.global _arch_RTOSINT
+	.global _arch_start_first_task
+	.global _arch_test_and_set
 	
 SAVE_CONTEXT .macro
 	;;  isr relevant registers are auto-saved
@@ -40,31 +52,31 @@ RESTORE_CONTEXT .macro
 	;;  iret will unstack the rest
 	.endm
 
-arch_INT14:
+_arch_INT14:
 	;; tick interrupt
 	SAVE_CONTEXT
 
 	mov AR6, @SP
 	mov AR4, AR6
-	lcr picoRTOS_tick
+	lcr _picoRTOS_tick
 
 	mov @SP, AR4
 	RESTORE_CONTEXT
 	iret
 
-arch_RTOSINT:
+_arch_RTOSINT:
 	;; context switch interrupt
 	SAVE_CONTEXT
-	
+
 	mov AR6, @SP
 	mov AR4, AR6
-	lcr picoRTOS_switch_context
+	lcr _picoRTOS_switch_context
 
 	mov @SP, AR4
 	RESTORE_CONTEXT
 	iret
 
-arch_start_first_task:
+_arch_start_first_task:
 	mov @SP, AR4
 	RESTORE_CONTEXT
 	;; emulate first iret
@@ -78,13 +90,13 @@ arch_start_first_task:
 	subb SP, #2		; ignore ACC
 	pop  T:ST0
 	;; don't forget LRETR will unstack RPC
-	movl XAR0, #arch_start_first_task
+	movl XAR0, #_arch_start_first_task
 	push XAR0
 	;; enable interrupts
 	clrc INTM
 	lretr
 
-arch_test_and_set:
+_arch_test_and_set:
 	mov AL, #1
 	tset *AR4, #0
 	sb arch_test_and_set_return, TC
