e174549e5d778893b951b95dcfccc21035fda43c
rickxxxblaine <Joachim Naulet>
2022-09-03 11:50:12 +0200

    demo: split raspberry pico into single core & SMP

--- demo/raspberry-pico-SMP/picoRTOSConfig.h
+++ demo/raspberry-pico-SMP/picoRTOSConfig.h
@@ -6,12 +6,17 @@
 #define CONFIG_TICK_HZ          1000
 
 /* TASKS */
-#define CONFIG_TASK_COUNT      2
+#define CONFIG_TASK_COUNT      3
 #define TASK_TICK_PRIO         0
-#define TASK_LED_PRIO          1
+#define TASK_LED0_PRIO         1
+#define TASK_LED1_PRIO         2
 
 /* STACK */
 #define CONFIG_DEFAULT_STACK_COUNT 256
+
+/* SMP */
+#define CONFIG_SMP_CORES          2
+#define CONFIG_DEADLOCK_COUNT     1000
 
 /* DEBUG */
 #define CONFIG_CHECK_STACK_INTEGRITY
